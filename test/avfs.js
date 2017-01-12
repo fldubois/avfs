8835c0e14dea462e5a2c07ea980e2446d32bb3a3
Florent Dubois <florent.dubois@devaddict.io>
2017-01-12 23:09:05 +0100

    Add truncateSync method

--- test/avfs.js
+++ test/avfs.js
@@ -151,4 +151,61 @@ describe('avfs', function () {
 
   });
 
+  describe('truncateSync()', function () {
+
+    it('should truncate file', function () {
+      var content = new Buffer('Hello, friend.');
+
+      fs.files = {'tmp': {'file.txt': content}};
+
+      var result = fs.truncateSync('/tmp/file.txt');
+
+      expect(result).to.be.an('undefined');
+      expect(fs.files.tmp['file.txt']).to.be.an.instanceof(Buffer);
+      expect(fs.files.tmp['file.txt'].length).to.equal(0);
+    });
+
+    it('should truncate file to the specified length', function () {
+      var content = new Buffer('Hello, friend.');
+
+      fs.files = {'tmp': {'file.txt': content}};
+
+      var result = fs.truncateSync('/tmp/file.txt', 3);
+
+      expect(result).to.be.an('undefined');
+      expect(fs.files.tmp['file.txt']).to.be.an.instanceof(Buffer);
+      expect(fs.files.tmp['file.txt'].length).to.equal(3);
+      expect(fs.files.tmp['file.txt'].toString()).to.equal(content.slice(0, 3).toString());
+    });
+
+    it('should throw on non existing path', function () {
+      expect(function () {
+        fs.truncateSync('/tmp/file.txt');
+      }).to.throw(Error, 'Error: ENOENT, no such file or directory \'/tmp/file.txt\'');
+    });
+
+    it('should throw on directory', function () {
+      fs.files = {'tmp': {'file.txt': new Buffer('Hello, friend.')}};
+
+      expect(function () {
+        fs.truncateSync('/tmp');
+      }).to.throw(Error, 'Error: EISDIR, illegal operation on a directory \'/tmp\'');
+    });
+
+    it('should throw on non string path', function () {
+      expect(function () {
+        fs.truncateSync(true);
+      }).to.throw(TypeError, 'path must be a string');
+    });
+
+    it('should throw on non integer length', function () {
+      fs.files = {'tmp': {'file.txt': new Buffer('Hello, friend.')}};
+
+      expect(function () {
+        fs.truncateSync('/tmp/file.txt', true);
+      }).to.throw(TypeError, 'Not an integer');
+    });
+
+  });
+
 });
