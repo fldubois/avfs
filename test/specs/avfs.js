3f7895ef7bf3ae397e94e3f811bac85acda5bee6
Florent Dubois <florent.dubois@devaddict.io>
2017-03-07 05:52:41 +0100

    Add chownSync method

--- test/specs/avfs.js
+++ test/specs/avfs.js
@@ -173,6 +173,62 @@ describe('avfs', function () {
 
   });
 
+  describe('chownSync()', function () {
+
+    it('should change the owner and group', function () {
+      var result = fs.chownSync('/tmp/file', 1001, 1001);
+
+      expect(result).to.be.an('undefined');
+      expect(fs.files.tmp.file['@uid']).to.equal(1001);
+      expect(fs.files.tmp.file['@gid']).to.equal(1001);
+    });
+
+    it('should follow symlinks', function () {
+      var result = fs.chownSync('/dir/link', 1001, 1001);
+
+      expect(result).to.be.an('undefined');
+      expect(fs.files.tmp.file['@uid']).to.equal(1001);
+      expect(fs.files.tmp.file['@gid']).to.equal(1001);
+    });
+
+    it('should throw on bad path parameter type', function () {
+      expect(function () {
+        fs.chownSync(false, 1001, 1001);
+      }).to.throw(TypeError, 'path must be a string');
+    });
+
+    it('should throw on bad uid parameter type', function () {
+      expect(function () {
+        fs.chownSync('/tmp/file', false, 1001);
+      }).to.throw(TypeError, 'uid must be an unsigned int');
+    });
+
+    it('should throw on bad gid parameter type', function () {
+      expect(function () {
+        fs.chownSync('/tmp/file', 1001, false);
+      }).to.throw(TypeError, 'gid must be an unsigned int');
+    });
+
+    it('should throw on non existing file', function () {
+      expect(function () {
+        fs.chownSync('/file', 1001, 1001);
+      }).to.throw(Error, 'ENOENT, no such file or directory \'/file\'');
+    });
+
+    it('should throw on non existing parent directory', function () {
+      expect(function () {
+        fs.chownSync('/not/file', 1001, 1001);
+      }).to.throw(Error, 'ENOENT, no such file or directory \'/not/file\'');
+    });
+
+    it('should throw on not directory parent', function () {
+      expect(function () {
+        fs.chownSync('/tmp/file/new', 1001, 1001);
+      }).to.throw(Error, 'ENOTDIR, not a directory \'/tmp/file/new\'');
+    });
+
+  });
+
   describe('closeSync()', function () {
 
     it('should close the file handle', function () {
