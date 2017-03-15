f56a17a8b5a04ce24e45ea78927a3e03b2b733c6
Florent Dubois <florent.dubois@devaddict.io>
2017-03-15 22:36:30 +0100

    Add futimesSync method

--- test/specs/avfs.js
+++ test/specs/avfs.js
@@ -895,6 +895,78 @@ describe('avfs', function () {
 
   });
 
+  describe('utimesSync()', function () {
+
+    it('should change timestamps', function () {
+      var date = new Date('2012-12-21T00:00:00.000Z');
+      var fd   = 10;
+
+      fs.handles[fd] = new Descriptor(getElement('/tmp/file'), '/tmp/file', flags.RW);
+
+      var result = fs.futimesSync(fd, date, date);
+
+      expect(result).to.be.an('undefined');
+      expect(getElement('/tmp/file').get('atime').getTime()).to.equal(date.getTime());
+      expect(getElement('/tmp/file').get('mtime').getTime()).to.equal(date.getTime());
+    });
+
+    it('should accept time as unix timestamp', function () {
+      var date      = new Date('2012-12-21T00:00:00.000Z');
+      var timestamp = date.getTime() / 1000;
+      var fd        = 10;
+
+      fs.handles[fd] = new Descriptor(getElement('/tmp/file'), '/tmp/file', flags.RW);
+
+      var result = fs.futimesSync(fd, timestamp, timestamp);
+
+      expect(result).to.be.an('undefined');
+      expect(getElement('/tmp/file').get('atime').getTime()).to.equal(date.getTime());
+      expect(getElement('/tmp/file').get('mtime').getTime()).to.equal(date.getTime());
+    });
+
+    it('should follow symlinks', function () {
+      var date = new Date('2012-12-21T00:00:00.000Z');
+      var fd   = 10;
+
+      fs.handles[fd] = new Descriptor(getElement('/dir/link'), '/dir/link', flags.RW);
+
+      var result = fs.futimesSync(fd, date, date);
+
+      expect(result).to.be.an('undefined');
+
+      expect(getElement('/dir/link').get('atime').getTime()).to.not.equal(date.getTime());
+      expect(getElement('/dir/link').get('mtime').getTime()).to.not.equal(date.getTime());
+
+      expect(getElement('/tmp/file').get('atime').getTime()).to.equal(date.getTime());
+      expect(getElement('/tmp/file').get('mtime').getTime()).to.equal(date.getTime());
+    });
+
+    it('should throw on non existing file descriptor', function () {
+      expect(function () {
+        fs.futimesSync(0, 0, 0);
+      }).to.throw(Error, 'EBADF, bad file descriptor');
+    });
+
+    it('should throw on non integer file descriptor', function () {
+      expect(function () {
+        fs.futimesSync(false, 0, 0);
+      }).to.throw(TypeError, 'Bad argument');
+    });
+
+    it('should throw bad atime parameter type', function () {
+      expect(function () {
+        fs.futimesSync(0, false, 0);
+      }).to.throw(Error, 'Cannot parse time: false');
+    });
+
+    it('should throw bad mtime parameter type', function () {
+      expect(function () {
+        fs.futimesSync(0, 0, false);
+      }).to.throw(Error, 'Cannot parse time: false');
+    });
+
+  });
+
   describe('lchmodSync()', function () {
 
     it('should change the mode', function () {
