38277ceb21efb77c265c89bfdd87c2c9ee55f6f1
Florent Dubois <florent.dubois@devaddict.io>
2017-03-18 22:42:56 +0100

    Add Stats prototype

---
+++ test/specs/common/stats.js
@@ -0,0 +1,120 @@
+'use strict';
+
+var chai   = require('chai');
+var expect = chai.expect;
+
+var elements = require('lib/common/elements');
+
+var Stats = require('lib/common/stats');
+
+var elems = {
+  directory: elements.directory(parseInt('777', 8)),
+  file:      elements.file(parseInt('777', 8), 'Hello'),
+  symlink:   elements.symlink(parseInt('777', 8), '/')
+};
+
+describe('common/stats', function () {
+
+  it('should expose a constructor', function () {
+    expect(Stats).to.be.a('function');
+    expect(new Stats(elems.file)).to.be.an.instanceOf(Stats);
+  });
+
+  it('should create stats from element', function () {
+    var stats = new Stats(elems.file);
+
+    expect(stats.dev).to.equal(1);
+    expect(stats.ino).to.equal(elems.file.get('inode'));
+    expect(stats.mode).to.equal(elems.file.get('mode'));
+    expect(stats.nlink).to.equal(elems.file.get('nlink'));
+    expect(stats.uid).to.equal(elems.file.get('uid'));
+    expect(stats.gid).to.equal(elems.file.get('gid'));
+    expect(stats.rdev).to.equal(0);
+    expect(stats.size).to.equal(0);
+    expect(stats.blksize).to.equal(512);
+    expect(stats.blocks).to.equal(0);
+    expect(stats.atime).to.equal(elems.file.get('atime'));
+    expect(stats.mtime).to.equal(elems.file.get('mtime'));
+    expect(stats.ctime).to.equal(elems.file.get('ctime'));
+  });
+
+  describe('isDirectory()', function () {
+
+    it('should return true on directory', function () {
+      expect(new Stats(elems.directory).isDirectory()).to.equal(true);
+    });
+
+    it('should return false on other types', function () {
+      expect(new Stats(elems.file).isDirectory()).to.equal(false);
+      expect(new Stats(elems.symlink).isDirectory()).to.equal(false);
+    });
+
+  });
+
+  describe('isFile()', function () {
+
+    it('should return true on file', function () {
+      expect(new Stats(elems.file).isFile()).to.equal(true);
+    });
+
+    it('should return false on other types', function () {
+      expect(new Stats(elems.directory).isFile()).to.equal(false);
+      expect(new Stats(elems.symlink).isFile()).to.equal(false);
+    });
+
+  });
+
+  describe('isBlockDevice()', function () {
+
+    it('should return false on all types', function () {
+      expect(new Stats(elems.directory).isBlockDevice()).to.equal(false);
+      expect(new Stats(elems.file).isBlockDevice()).to.equal(false);
+      expect(new Stats(elems.symlink).isBlockDevice()).to.equal(false);
+    });
+
+  });
+
+  describe('isCharacterDevice()', function () {
+
+    it('should return false on all types', function () {
+      expect(new Stats(elems.directory).isCharacterDevice()).to.equal(false);
+      expect(new Stats(elems.file).isCharacterDevice()).to.equal(false);
+      expect(new Stats(elems.symlink).isCharacterDevice()).to.equal(false);
+    });
+
+  });
+
+  describe('isSymbolicLink()', function () {
+
+    it('should return true on symlink', function () {
+      expect(new Stats(elems.symlink).isSymbolicLink()).to.equal(true);
+    });
+
+    it('should return false on other types', function () {
+      expect(new Stats(elems.directory).isSymbolicLink()).to.equal(false);
+      expect(new Stats(elems.file).isSymbolicLink()).to.equal(false);
+    });
+
+  });
+
+  describe('isFIFO()', function () {
+
+    it('should return false on all types', function () {
+      expect(new Stats(elems.directory).isFIFO()).to.equal(false);
+      expect(new Stats(elems.file).isFIFO()).to.equal(false);
+      expect(new Stats(elems.symlink).isFIFO()).to.equal(false);
+    });
+
+  });
+
+  describe('isSocket()', function () {
+
+    it('should return false on all types', function () {
+      expect(new Stats(elems.directory).isSocket()).to.equal(false);
+      expect(new Stats(elems.file).isSocket()).to.equal(false);
+      expect(new Stats(elems.symlink).isSocket()).to.equal(false);
+    });
+
+  });
+
+});
