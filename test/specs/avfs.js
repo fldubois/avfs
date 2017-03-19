3d4be963211752d432c4712f0160ad25e94e961e
Florent Dubois <florent.dubois@devaddict.io>
2017-03-19 19:25:05 +0100

    Add statSync method

--- test/specs/avfs.js
+++ test/specs/avfs.js
@@ -11,6 +11,7 @@ var storage  = require('lib/common/storage');
 
 var AVFS       = require('lib/avfs');
 var Descriptor = require('lib/common/descriptor');
+var Stats      = require('lib/common/stats');
 
 var fs = new AVFS();
 
@@ -45,6 +46,10 @@ describe('avfs', function () {
 
     fs.handles = {};
     fs.next    = 0;
+  });
+
+  it.only('should expose Stats', function () {
+    expect(fs.Stats).to.equal(Stats);
   });
 
   describe('appendFileSync()', function () {
@@ -1657,6 +1662,57 @@ describe('avfs', function () {
     it('should throw on non string path', function () {
       expect(function () {
         fs.rmdirSync(true);
+      }).to.throw(TypeError, 'path must be a string');
+    });
+
+  });
+
+  describe('statSync()', function () {
+
+    it('should return file stats', function () {
+      var stats = fs.statSync('/tmp/file');
+
+      expect(stats).to.be.an('object');
+
+      var file = getElement('/tmp/file');
+
+      expect(stats.dev).to.equal(1);
+      expect(stats.ino).to.equal(file.get('inode'));
+      expect(stats.mode).to.equal(file.get('mode'));
+      expect(stats.nlink).to.equal(file.get('nlink'));
+      expect(stats.uid).to.equal(file.get('uid'));
+      expect(stats.gid).to.equal(file.get('gid'));
+      expect(stats.rdev).to.equal(0);
+      expect(stats.size).to.equal(0);
+      expect(stats.blksize).to.equal(512);
+      expect(stats.blocks).to.equal(0);
+      expect(stats.atime).to.equal(file.get('atime'));
+      expect(stats.mtime).to.equal(file.get('mtime'));
+      expect(stats.ctime).to.equal(file.get('ctime'));
+    });
+
+    it('should follow symlinks', function () {
+      var statsFile = fs.statSync('/tmp/file');
+      var statsLink = fs.statSync('/dir/link');
+
+      expect(statsFile).to.deep.equal(statsLink);
+    });
+
+    it('should throw on non existing path', function () {
+      expect(function () {
+        fs.statSync('/not/test.txt');
+      }).to.throw(Error, 'ENOENT, no such file or directory \'/not/test.txt\'');
+    });
+
+    it('should throw on non directory parent', function () {
+      expect(function () {
+        fs.statSync('/tmp/file/file');
+      }).to.throw(Error, 'ENOTDIR, not a directory \'/tmp/file/file\'');
+    });
+
+    it('should throw on non string path', function () {
+      expect(function () {
+        fs.statSync(true);
       }).to.throw(TypeError, 'path must be a string');
     });
 
