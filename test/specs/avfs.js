1b232bf83b6fed68942ca7e9db23b394af1f2979
Florent Dubois <florent.dubois@devaddict.io>
2017-03-20 22:03:40 +0100

    Add lstatSync method

--- test/specs/avfs.js
+++ test/specs/avfs.js
@@ -48,7 +48,7 @@ describe('avfs', function () {
     fs.next    = 0;
   });
 
-  it.only('should expose Stats', function () {
+  it('should expose Stats', function () {
     expect(fs.Stats).to.equal(Stats);
   });
 
@@ -1140,6 +1140,57 @@ describe('avfs', function () {
       expect(function () {
         fs.linkSync('/tmp/file', true);
       }).to.throw(TypeError, 'src path must be a string');
+    });
+
+  });
+
+  describe('lstatSync()', function () {
+
+    it('should return file stats', function () {
+      var stats = fs.lstatSync('/tmp/file');
+
+      expect(stats).to.be.an('object');
+
+      var file = getElement('/tmp/file');
+
+      expect(stats.dev).to.equal(1);
+      expect(stats.ino).to.equal(file.get('inode'));
+      expect(stats.mode).to.equal(file.get('mode'));
+      expect(stats.nlink).to.equal(file.get('nlink'));
+      expect(stats.uid).to.equal(file.get('uid'));
+      expect(stats.gid).to.equal(file.get('gid'));
+      expect(stats.rdev).to.equal(0);
+      expect(stats.size).to.equal(0);
+      expect(stats.blksize).to.equal(512);
+      expect(stats.blocks).to.equal(0);
+      expect(stats.atime).to.equal(file.get('atime'));
+      expect(stats.mtime).to.equal(file.get('mtime'));
+      expect(stats.ctime).to.equal(file.get('ctime'));
+    });
+
+    it('should not follow symlinks', function () {
+      var statsFile = fs.lstatSync('/tmp/file');
+      var statsLink = fs.lstatSync('/dir/link');
+
+      expect(statsFile).to.not.deep.equal(statsLink);
+    });
+
+    it('should throw on non existing path', function () {
+      expect(function () {
+        fs.lstatSync('/not/test.txt');
+      }).to.throw(Error, 'ENOENT, no such file or directory \'/not/test.txt\'');
+    });
+
+    it('should throw on non directory parent', function () {
+      expect(function () {
+        fs.lstatSync('/tmp/file/file');
+      }).to.throw(Error, 'ENOTDIR, not a directory \'/tmp/file/file\'');
+    });
+
+    it('should throw on non string path', function () {
+      expect(function () {
+        fs.lstatSync(true);
+      }).to.throw(TypeError, 'path must be a string');
     });
 
   });
