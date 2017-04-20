1bb484ae0c904d1c95914c53f9b9e030b7f48c5f
Florent Dubois <florent.dubois@devaddict.io>
2017-04-20 22:58:16 +0200

    Run common specs only for supported node versions

---
+++ test/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var fs   = require('fs');
+var path = require('path');
+
+var chai   = require('chai');
+var expect = chai.expect;
+
+var version = require('../lib/common/version');
+
+var supported = fs.readdirSync(path.join(__dirname, '../lib'));
+
+if (supported.indexOf(version) === -1) {
+
+  describe('avfs', function () {
+
+    it('should throw unsupported version error', function () {
+      expect(function () {
+        require('lib/avfs');
+      }).to.throw(Error, 'Unsupported node version: ' + process.version);
+    });
+
+  });
+
+} else {
+  var specs = fs.readdirSync(path.join(__dirname, 'specs/common')).map(function (filename) {
+    return path.join('test/specs/common', filename);
+  });
+
+  specs.unshift('test/specs/avfs');
+
+  specs.forEach(require);
+}
--- test/specs/avfs/index.js
+++ test/specs/avfs/index.js
@@ -1,108 +1,64 @@
 'use strict';
 
-var chai   = require('chai');
-var expect = chai.expect;
+var fs   = require('fs');
+var path = require('path');
 
 var elements = require('lib/common/elements');
 var storage  = require('lib/common/storage');
 var version  = require('lib/common/version');
 
-var specs = [
-  './partials/members',
-  './partials/methods/accessSync',
-  './partials/methods/appendFileSync',
-  './partials/methods/chmodSync',
-  './partials/methods/chownSync',
-  './partials/methods/closeSync',
-  './partials/methods/createReadStream',
-  './partials/methods/createWriteStream',
-  './partials/methods/existsSync',
-  './partials/methods/fchmodSync',
-  './partials/methods/fchownSync',
-  './partials/methods/fstatSync',
-  './partials/methods/fsyncSync',
-  './partials/methods/ftruncateSync',
-  './partials/methods/futimesSync',
-  './partials/methods/lchmodSync',
-  './partials/methods/lchownSync',
-  './partials/methods/linkSync',
-  './partials/methods/lstatSync',
-  './partials/methods/mkdirSync',
-  './partials/methods/openSync',
-  './partials/methods/readdirSync',
-  './partials/methods/readFileSync',
-  './partials/methods/readlinkSync',
-  './partials/methods/readSync',
-  './partials/methods/realpathSync',
-  './partials/methods/renameSync',
-  './partials/methods/rmdirSync',
-  './partials/methods/statSync',
-  './partials/methods/symlinkSync',
-  './partials/methods/truncateSync',
-  './partials/methods/unlinkSync',
-  './partials/methods/utimesSync',
-  './partials/methods/writeFileSync',
-  './partials/methods/writeSync',
-  './partials/async'
-];
+var AVFS = require('lib/avfs');
+
+var avfs = new AVFS();
+
+var getElement = function (filepath) {
+  var current = avfs.files;
+
+  storage.parse(filepath).forEach(function (element) {
+    current = current.get('content')[element];
+  });
+
+  return current;
+};
+
+var specs = fs.readdirSync(path.join(__dirname, 'partials/methods')).map(function (filename) {
+  return './partials/methods/' + filename;
+});
+
+specs.unshift('./partials/members');
+specs.push('./partials/async');
 
 describe('avfs', function () {
 
-  if (['v0.10', 'v0.12'].indexOf(version) === -1) {
+  beforeEach('reset virtual file system', function () {
+    var otherFile = elements.file(parseInt('0666', 8), new Buffer('Hello, friend.'));
 
-    it('should throw unsupported version error', function () {
-      expect(function () {
-        require('lib/avfs');
-      }).to.throw(Error, 'Unsupported node version: ' + process.version);
+    otherFile.set('uid', process.getuid() + 1);
+
+    avfs.files = elements.directory(parseInt('0755', 8), {
+      tmp: elements.directory(parseInt('0777', 8), {
+        ascii: elements.file(parseInt('0666', 8), new Buffer('Hello, friend.')),
+        empty: elements.file(parseInt('0666', 8), new Buffer(0)),
+        file:  elements.file(parseInt('0666', 8), new Buffer('Hello, friend.'))
+      }),
+      dir: elements.directory(parseInt('0777', 8), {
+        link:  elements.symlink(parseInt('0777', 8), '/tmp/file'),
+        dlink: elements.symlink(parseInt('0777', 8), '/dir'),
+        perm:  elements.file(parseInt('0000', 8), new Buffer('Hello, friend.')),
+        other: otherFile
+      }),
+      perm: elements.directory(parseInt('0000', 8), {
+        file: elements.file(parseInt('0666', 8), new Buffer('Hello, friend.')),
+        dir:  elements.directory(parseInt('0777', 8))
+      })
     });
 
-  } else {
+    avfs.handles = {};
+    avfs.next    = 0;
+  });
 
-    var AVFS = require('lib/avfs');
-
-    var fs = new AVFS();
-
-    var getElement = function (path) {
-      var current = fs.files;
-
-      storage.parse(path).forEach(function (element) {
-        current = current.get('content')[element];
-      });
-
-      return current;
-    };
-
-    beforeEach('reset virtual file system', function () {
-      var otherFile = elements.file(parseInt('0666', 8), new Buffer('Hello, friend.'));
-
-      otherFile.set('uid', process.getuid() + 1);
-
-      fs.files = elements.directory(parseInt('0755', 8), {
-        tmp: elements.directory(parseInt('0777', 8), {
-          ascii: elements.file(parseInt('0666', 8), new Buffer('Hello, friend.')),
-          empty: elements.file(parseInt('0666', 8), new Buffer(0)),
-          file:  elements.file(parseInt('0666', 8), new Buffer('Hello, friend.'))
-        }),
-        dir: elements.directory(parseInt('0777', 8), {
-          link:  elements.symlink(parseInt('0777', 8), '/tmp/file'),
-          dlink: elements.symlink(parseInt('0777', 8), '/dir'),
-          perm:  elements.file(parseInt('0000', 8), new Buffer('Hello, friend.')),
-          other: otherFile
-        }),
-        perm: elements.directory(parseInt('0000', 8), {
-          file: elements.file(parseInt('0666', 8), new Buffer('Hello, friend.')),
-          dir:  elements.directory(parseInt('0777', 8))
-        })
-      });
-
-      fs.handles = {};
-      fs.next    = 0;
-    });
-
-    specs.forEach(function (spec) {
-      require(spec)(fs, getElement, version);
-    });
-
-  }
+  specs.forEach(function (spec) {
+    require(spec)(avfs, getElement, version);
+  });
 
 });
