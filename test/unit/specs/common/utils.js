dcf901bbd26fe002f2cbe4aab0cce9f68e690f6c
Florent Dubois <florent.dubois@devaddict.io>
2017-06-27 23:07:12 +0200

    Remove dependency on errors of basic functions

--- test/unit/specs/common/utils.js
+++ test/unit/specs/common/utils.js
@@ -6,6 +6,8 @@ var sinon  = require('sinon');
 
 var utils = require('lib/common/utils');
 
+var AVFSError = require('lib/common/avfs-error');
+
 chai.use(require('sinon-chai'));
 
 // Target mock
@@ -101,6 +103,66 @@ describe('common/utils', function () {
 
     after(function () {
       console.error.restore();
+    });
+
+  });
+
+  describe('invoke()', function () {
+
+    var method = sinon.stub();
+
+    beforeEach(function () {
+      method.reset();
+    });
+
+    it('should call the function with parameters', function () {
+      method.onCall(0).returns(0);
+      method.onCall(1).returns(1);
+      method.onCall(2).returns(2);
+
+      expect(utils.invoke(method, [1, true, 'test'])).to.equal(0);
+      expect(utils.invoke(method, [false, 2])).to.equal(1);
+      expect(utils.invoke(method)).to.equal(2);
+
+      expect(method).to.have.callCount(3);
+
+      expect(method.firstCall).to.have.been.calledWithExactly(1, true, 'test');
+      expect(method.secondCall).to.have.been.calledWithExactly(false, 2);
+      expect(method.thirdCall).to.have.been.calledWithExactly();
+    });
+
+    it('should call transform on AVFSError', function () {
+      var error = new Error('Fake error');
+
+      method.throws(new AVFSError('ENOENT'));
+
+      var callback = sinon.stub();
+
+      callback.throws(error);
+
+      expect(function () {
+        utils.invoke(method, [true], {
+          ENOENT: callback
+        });
+      }).to.throw(error);
+
+      expect(callback).to.have.callCount(1);
+    });
+
+    it('should throw errors', function () {
+      var error = new Error('Fake error');
+
+      method.throws(error);
+
+      var callback = sinon.stub();
+
+      expect(function () {
+        utils.invoke(method, [true], {
+          ENOENT: callback
+        });
+      }).to.throw(error);
+
+      expect(callback).to.have.callCount(0);
     });
 
   });
