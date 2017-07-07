a0ff7e3a1bacfd773f106a4fae2a7b5a31374865
Florent Dubois <florent.dubois@devaddict.io>
2017-07-07 22:41:02 +0200

    Move unit tests for permissions basic functions

---
+++ test/unit/specs/common/avfs/permissions.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var chai   = require('chai');
+var expect = chai.expect;
+
+var elements = require('lib/common/elements');
+var factory  = require('lib/common/avfs/permissions');
+
+var Storage = require('lib/common/storage');
+
+describe('common/avfs/permissions', function () {
+
+  var uid = process.getuid();
+  var gid = process.getgroups()[0];
+
+  var storage = new Storage();
+
+  var base = factory(storage);
+
+  before(function () {
+    var file = elements.file(parseInt('0666', 8), new Buffer('Hello, friend.'));
+    var link = elements.symlink(parseInt('0777', 8), '/file');
+
+    file.set('gid', gid + 1);
+    link.set('gid', gid + 1);
+
+    storage.files = elements.directory(parseInt('0755', 8), {
+      file: file,
+      link: link
+    });
+  });
+
+  describe('chmod()', function () {
+
+    it('should change the mode', function () {
+      var result = base.chmod('/file', '0700');
+
+      expect(result).to.be.an('undefined');
+      expect(storage.files).to.contain.an.avfs.file('/file').with.mode('0700');
+    });
+
+    it('should follow symlinks', function () {
+      var result = base.chmod('/link', '0700');
+
+      expect(result).to.be.an('undefined');
+      expect(storage.files).to.contain.an.avfs.file('/file').with.mode('0700');
+      expect(storage.files).to.contain.an.avfs.symlink('/link').with.mode('0777');
+    });
+
+  });
+
+  describe('chown()', function () {
+
+    it('should change the owner and group', function () {
+      var result = base.chown('/file', uid, gid);
+
+      expect(result).to.be.an('undefined');
+      expect(storage.files).to.contain.an.avfs.file('/file').with.owner(uid, gid);
+    });
+
+    it('should follow symlinks', function () {
+      var result = base.chown('/link', uid, gid);
+
+      expect(result).to.be.an('undefined');
+      expect(storage.files).to.contain.an.avfs.file('/file').with.owner(uid, gid);
+      expect(storage.files).to.contain.an.avfs.symlink('/link').with.owner(uid, gid + 1);
+    });
+
+  });
+
+});
